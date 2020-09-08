82c667f6ae89549ad72d1f6067650d37f242e979
Jincan Lin <34576058+ihotarubi@users.noreply.github.com>
2020-09-08 11:16:35 +0800

    环境配置测试

---
+++ firstOpencv/firstOpencv/main.cpp
@@ -0,0 +1,16 @@
+#include <opencv2\opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	Mat src = imread("../../sample/lena512color.bmp");
+
+	imshow("input", src);
+
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
